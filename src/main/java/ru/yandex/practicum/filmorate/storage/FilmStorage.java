75afd9b18299dcf06c34e5412ff00553481a172a
Kinza <kira-kazan1997@yandex.ru>
2026-06-16 10:02:37 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film save(Film film);
+
     Film update(Film film);
+
     Optional<Film> findById(Integer id);
+
     List<Film> findAll();
+
     void deleteById(Integer id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User save(User user);
+
     User update(User user);
+
     Optional<User> findById(Integer id);
+
     List<User> findAll();
+
     void deleteById(Integer id);
 }
